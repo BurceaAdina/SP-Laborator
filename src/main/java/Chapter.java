4f200dd669095032a140282f08de89e6a9d89d7c
BurceaAdina <73740440+BurceaAdina@users.noreply.github.com>
2021-11-08 01:22:09 +0200

    Update Chapter.java

--- src/main/java/Chapter.java
+++ src/main/java/Chapter.java
@@ -3,31 +3,15 @@ import java.util.List;
 
 public class Chapter {
     private String name;
-    private Book book;
     private List<SubChapter> subChapters = new ArrayList<>();
-
-    public Chapter() {
-
-    }
 
     public Chapter(String name) {
         this.name = name;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public Book getBook() {
-        return book;
-    }
 
     public List<SubChapter> getSubChapters() {
         return subChapters;
-    }
-
-    public void setSubChapters(List<SubChapter> subChapters) {
-        this.subChapters = subChapters;
     }
 
     public int createSubChapter(String element) {
@@ -40,6 +24,10 @@ public class Chapter {
         return subChapters.get(indexSubChapterOneOne);
     }
     public void print() {
+        System.out.println("Chapter " + name);
+		for (SubChapter sc : subChapters) {
+			sc.print();
+		}
 
     }
 }
