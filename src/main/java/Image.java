bd9b4ed6f0acb8973e36e68641df34cc47df4a6d
BurceaAdina <73740440+BurceaAdina@users.noreply.github.com>
2021-11-08 01:27:35 +0200

    Update Image.java

--- src/main/java/Image.java
+++ src/main/java/Image.java
@@ -1,22 +1,11 @@
-public class Image {
+public class Image extends Element{
     private String imageName;
-    private SubChapter subChapter;
-    public Image() {
-
-    }
 
     public Image(String imageName) {
         this.imageName = imageName;
     }
 
-    public String getImageName() {
-        return imageName;
-    }
-
-    public SubChapter getSubChapter() {
-        return subChapter;
-    }
-
     public void print() {
+        System.out.println("Image with name:" + imageName);
     }
 }
