2a68d94c8bfb83fddcb5ff0f34591f7d33ef8575
BurceaAdina <73740440+BurceaAdina@users.noreply.github.com>
2021-11-08 01:31:47 +0200

    Update Paragraph.java

--- src/main/java/Paragraph.java
+++ src/main/java/Paragraph.java
@@ -1,22 +1,11 @@
-public class Paragraph {
+public class Paragraph extends Element {
     private String text;
-    private SubChapter subChapter;
-    public Paragraph() {
-
-    }
 
     public Paragraph(String text) {
         this.text = text;
     }
 
-    public String getText() {
-        return text;
-    }
-
-    public SubChapter getSubChapter() {
-        return subChapter;
-    }
-
     public void print() {
+        System.out.println("Paragraph: " + text);
     }
 }
