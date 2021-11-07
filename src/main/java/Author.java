6690eca92a215d2d5b21b1ebc2f5574a3aa2f958
BurceaAdina <73740440+BurceaAdina@users.noreply.github.com>
2021-11-08 01:08:24 +0200

    Update Author.java

--- src/main/java/Author.java
+++ src/main/java/Author.java
@@ -1,20 +1,9 @@
 public class Author {
     private String name;
-    private Book book;
-    public Author() {
-    }
-
     public Author(String name) {
         this.name = name;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public Book getBook() {
-        return book;
-    }
     public void print() {
 
     }
