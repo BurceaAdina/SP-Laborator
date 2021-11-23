c2ec819a9acbb65835e86f48ec122191c53dc88d
BurceaAdina <73740440+BurceaAdina@users.noreply.github.com>
2021-11-23 20:15:07 +0200

    Add files via upload
    A mai fost pus pe branch-ul de master dar nu l-ati vazut.

---
+++ Lab2/src/Image.java
@@ -0,0 +1,11 @@
+public class Image extends Element {
+	private String imageName;
+	
+	Image(String name) {
+		imageName = name;
+	}
+	
+	public void print() {
+		System.out.println("Image with name:" + imageName);
+	}
+}
---
+++ Lab2/src/Paragraph.java
@@ -0,0 +1,13 @@
+public class Paragraph extends Element {
+	private String text;
+	
+	Paragraph(String paragraphText) {
+		text = paragraphText;
+	}
+	
+	public void print() {
+		System.out.println("Paragraph: " + text);
+	}
+	
+
+}
